c966be35b954347de26cc8179ff14d06abaf621b
aliasjuhi <99997053+aliasjuhi@users.noreply.github.com>
2023-02-19 17:51:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const routes = require('./routes');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 
 // Define API routes here
 app.use(routes);
